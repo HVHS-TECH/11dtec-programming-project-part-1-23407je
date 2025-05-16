6c90b435c3bb230145832cd7ee3136ab45616642
23407je <23407je@hvhs.school.nz>
2025-05-16 04:43:25 +0000

    May 16, 2025, 4:43 AM

--- Rentalslots.js
+++ Rentalslots.js
@@ -12,7 +12,7 @@ var Roulete =(0);
 var CashSlots =(0);
 var HorseRacing =(0);
 var Cash =(0);
-const startingMinutes = 10;
+const startingMinutes = 1;
 let time = startingMinutes * 60
 const countdownEL = document.getElementById('countdown');
 setInterval(updateCountdown, 1000);
